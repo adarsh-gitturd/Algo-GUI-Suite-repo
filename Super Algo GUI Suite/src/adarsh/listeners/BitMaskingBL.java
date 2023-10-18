6e8c6daca0e0e10bf2f1af72e36c07aac243a876
Adarsh Wilson <adarshwilsona@gmail.com>
2023-10-19 00:09:12 +0530

    minor commits before a short break

--- Super Algo GUI Suite/src/adarsh/listeners/BitMaskingBL.java
+++ Super Algo GUI Suite/src/adarsh/listeners/BitMaskingBL.java
@@ -31,7 +31,7 @@ public class BitMaskingBL implements ActionListener{
 		if(b == buttons.get(1)) {
 			String ans;
 			
-			JTextField input = JOptionPane.showInputDialog(null, "Enter a number : ");  
+			//JTextField input = JOptionPane.showInputDialog(null, "Enter a number : ");  
 			String inpust = JOptionPane.showInputDialog(null, "Enter an index : ");  
 			//ans = app.getMenuButtonListener().getBitMasking().bma.getIthBit(Integer.parseInt(input));
 			JOptionPane.showMessageDialog(null, "");
