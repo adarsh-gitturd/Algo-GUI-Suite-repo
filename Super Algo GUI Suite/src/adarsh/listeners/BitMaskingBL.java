a0835ffac36c0bd64325b1cc82e9108ec741396d
Adarsh Wilson <adarshwilsona@gmail.com>
2023-10-19 22:08:46 +0530

    initial gui issues revisited

--- Super Algo GUI Suite/src/adarsh/listeners/BitMaskingBL.java
+++ Super Algo GUI Suite/src/adarsh/listeners/BitMaskingBL.java
@@ -3,15 +3,17 @@ package adarsh.listeners;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 
 import adarsh.main.App;
@@ -36,20 +38,26 @@ public class BitMaskingBL implements ActionListener{
 			JOptionPane.showMessageDialog(null, ans);
 		}
 		if(b == buttons.get(1)) {
-			String ans;
+			int ans;
 			JPanel panel = new JPanel();	
-			panel.setLayout(new GridLayout(6,2));
-//			panel.add(Box.createVerticalStrut(100));
-			panel.add(new JLabel("Enter a number : "));
-			panel.add(new JTextArea("enter smth"));
+			panel.setLayout(new GridLayout(6,5));
+			JLabel answer = new JLabel("fff");
+			
+		    panel.add(new JLabel("Enter a number : "));
+			JTextArea ta1 = new JTextArea(" ", 2, 1);
+			panel.add(ta1);
 			panel.add(new JLabel("Enter an index : "));
-			panel.add(new JTextArea("index smth"));	
-			int input = JOptionPane.showConfirmDialog(null, panel, 
-		               "Get Ith Bit of a number", JOptionPane.OK_CANCEL_OPTION);
-//			String inpust = JOptionPane.showInputDialog(null, "Enter a number : ");  
-//			String inpusdt = JOptionPane.showInputDialog(null, "Enter an index : ");  
-			//ans = app.getMenuButtonListener().getBitMasking().bma.getIthBit(Integer.parseInt(input));
-			JOptionPane.showMessageDialog(null, "");
+			JTextArea ta2 = new JTextArea(" ", 2, 1);
+			panel.add(ta2);
+			panel.add(answer);
+		
+			JOptionPane.showConfirmDialog(null, panel, 
+		               "Get Ith Bit of a number", JOptionPane.DEFAULT_OPTION);
+			System.out.println(ta1.getText());
+			ans = app.getMenuButtonListener().getBitMasking().bma.getIthBit
+					(10, 2);
+			answer.setText(ta1.getText());
+			//JOptionPane.showMessageDialog(null, "");
 		}
 	}
 	
